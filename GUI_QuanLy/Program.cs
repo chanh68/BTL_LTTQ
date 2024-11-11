2dacaf388fde11573ea25e5f592e8e1183e0bb58
Yuki Ramu <hailam2034@gmail.com>
2024-11-12 05:24:57 +0700

    Report

--- GUI_QuanLy/Program.cs
+++ GUI_QuanLy/Program.cs
@@ -16,7 +16,7 @@ namespace GUI_QuanLy
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Report_DoanhThu());
+            Application.Run(new Signin());
         }
     }
 }
